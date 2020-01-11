cbf32e92cc62018a0ef0773d96c10290715515ee
tanizawa_kenta <kt10025102@gmail.com>
2020-01-11 11:29:10 +0900

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,9 @@
-server '18.177.167.97', user: 'app', roles: %w{app db web}
-set :ssh_options, keys: '/Users/tanizawakenta/.ssh/id_rsa'
+# server '18.177.167.97', user: 'app', roles: %w{app db web}
+# set :ssh_options, keys: '/Users/tanizawakenta/.ssh/id_rsa'
+
+set :ssh_options, {
+  port: 22,　#XXXにポート番号
+  keys: [File.expand_path('/Users/tanizawakenta/.ssh/id_rsa')],　#'~/.ssh/id_rsa'部分をローカル環境の鍵のパスに
+  forward_agent: true,
+  auth_methods: %w(publickey)
+}
