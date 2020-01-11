5ef1a25fbe49366dd44ce5a03350318ae9895c6d
tanizawa_kenta <kt10025102@gmail.com>
2020-01-11 11:39:46 +0900

    update

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,9 @@
-# server '18.177.167.97', user: 'app', roles: %w{app db web}
-# set :ssh_options, keys: '/Users/tanizawakenta/.ssh/id_rsa'
+server '18.177.167.97', user: 'app', roles: %w{app db web}
+set :ssh_options, keys: '/Users/tanizawakenta/.ssh/id_rsa'
 
-set :ssh_options, {
-  port: 22,
-  keys: [File.expand_path('/Users/tanizawakenta/.ssh/id_rsa')],
-  forward_agent: true,
-  auth_methods: %w(publickey)
-}
+# set :ssh_options, {
+#   port: 22,
+#   keys: [File.expand_path('/Users/tanizawakenta/.ssh/id_rsa')],
+#   forward_agent: true,
+#   auth_methods: %w(publickey)
+# }
